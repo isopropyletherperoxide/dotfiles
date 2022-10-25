10095ee6d8496737ce8e1c2fabd6d07075ebd86e
endless.sine@gmail.com <endless.sine@gmail.com>
2022-10-25 23:23:44 +0300

    fishie shell

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+alias weather="curl wttr.in"
+alias neovim="~/.config/Scripts/nvim.sh"
+export EDITOR="nvim"
+end
---
+++ fish/functions/fish_greeting.fish
@@ -0,0 +1,4 @@
+function fish_greeting
+lolcat Documents/MOTD.txt
+task | lolcat
+end
